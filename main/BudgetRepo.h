66fc2d1275342fd236480967e45700445a4c8ccf
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-05-03 19:23:03 +0800

    :recycle: use budget loop to replace month loop

--- main/BudgetRepo.h
+++ main/BudgetRepo.h
@@ -21,9 +21,6 @@ public:
     BudgetRepo() {};
 
     BudgetRepo(const std::vector<Budget> &budgets) : budgets_(budgets) {
-        for (const auto &budget : budgets_) {
-            map_budgets_[budget.year_month_day] = budget;
-        }
     }
 
     int queryBudget(const date::year_month_day &start, const date::year_month_day &end) {
@@ -32,9 +29,12 @@ public:
 
         int budget_amount = 0;
 
-        for (year_month current = start.year() / start.month();
-             current <= end.year() / end.month(); current += months(1)) {
-            budget_amount += getDailyAmount(current / 1) * getOverlappingDayCount(start, end, current);
+        for (const auto &budget : budgets_) {
+            budget_amount +=
+                    budget.amount /
+                    DaysInMonth(budget.year_month_day.year() / budget.year_month_day.month() / 1) *
+                    getOverlappingDayCount(start, end,
+                                           budget.year_month_day.year() / budget.year_month_day.month());
         }
 
         return budget_amount;
@@ -50,19 +50,10 @@ private:
         return (overlappingEnd.day() - overlappingStart.day()).count() + 1;
     }
 
-    int getDailyAmount(const year_month_day &start) {
-        return getBudgetAmount(start.year(), start.month()) / DaysInMonth(start);
-    }
-
-    int &getBudgetAmount(const year &curr_year, const month &curr_month) {
-        return map_budgets_[curr_year / curr_month / 1].amount;
-    }
-
     int DaysInMonth(const date::year_month_day &ymd) {
         return (uint) (ymd.year() / ymd.month() / last).day();
     }
 
-    std::map<date::year_month_day, Budget> map_budgets_;
     std::vector<Budget> budgets_;
 };
 
